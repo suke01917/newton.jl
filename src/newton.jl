18a5bc4b031cc9b3638457e7b91f21331e5a6899
suke01917 <83210583+suke01917@users.noreply.github.com>
2022-11-17 14:05:07 +0900

    Add files via upload

--- src/newton.jl
+++ src/newton.jl
@@ -1,6 +1,49 @@
 module newton
 
-greet() = "Hello World!!"
-# Write your package code here.
+function newton(f)
+    i = 0
+    x = 1
+    h = 0.01
+    fd(x) = (f(x+h)-f(x-h))/2h
+
+    for i = 1:1000
+        xn = x - f(x)/fd(x)
+        if abs(xn - x) < 0.00001
+            x = xn
+            break
+        else 
+            x = xn 
+        end
+    end
+
+    print("\n")
+    return x
+end
+
+function newton_detail(f)
+    i = 0
+    x = parse(Float64, Base.prompt("start value"))
+    h = 0.01
+    fd(x) = (f(x+h)-f(x-h))/2h
+
+    times = parse(Int64, Base.prompt("times"))
+    limit = parse(Float64, Base.prompt("limit"))
+    for i = 1:times
+        xn = x - f(x)/fd(x)
+        println(i, " : ", xn)
+        if abs(xn - x) < limit
+            x = xn
+            break
+        else
+            x = xn 
+        end
+    end
+
+    print("\n")
+    return x
+end
+
+export newton
+export newton_detail
 
 end
